769ddd312264525ab066b57057e8c52c4637c5a4
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-25 12:43:38 +0100

    Update hooks.ts

---
+++ lib/auth/hooks.ts
@@ -0,0 +1,14 @@
+'use client';
+
+/**
+ * @deprecated Use '@/lib/features/auth' instead
+ */
+
+export { 
+  useAuth, 
+  useAuth as useAuthState,
+  useLogin, 
+  useLogout, 
+  useRegister,
+  useAuthActions,
+} from '@/lib/features/auth';
