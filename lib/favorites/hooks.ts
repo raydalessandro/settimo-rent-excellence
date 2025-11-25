8c580b9901b30577895981e120c005790e1ba0e9
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-25 12:44:01 +0100

    Update hooks.ts

---
+++ lib/favorites/hooks.ts
@@ -0,0 +1,16 @@
+'use client';
+
+/**
+ * @deprecated Use '@/lib/features/user' instead
+ */
+
+export { 
+  useFavorites,
+  useFavorites as useFavoriteVehicles,
+  useFavoritesCount,
+  useAddFavorite,
+  useRemoveFavorite,
+  useFavoriteStatus,
+  useFavoriteStatus as useIsFavorite,
+  useToggleFavorite,
+} from '@/lib/features/user';
